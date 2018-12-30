153dbd22ba1dbedf59d79360c5e3bb9d9fae9551
arslan <arslanafzal321@gmail.com>
2018-12-30 22:23:52 +0500

    verion 1.0.0 complete

---
+++ GermanUmlautsTyper.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name         German Umlauts Typer
+// @namespace    http://eresdev.com
+// @version      1.0.0
+// @description  Allows you to type German Umlauts easily with main keyboard button combinations without ust ALT+ NUM keys combination. Currently, it handles only works for Windows keyboard. It has not been tested for Mac.
+// @author       EresDev
+// @match        https://www.memrise.com/course/*
+// @grant        none
+// ==/UserScript==
+
+function liveInputWatch(){
+    setInterval(function(){
+        let input_elements = document.getElementsByTagName("input");
+
+        for(let i = 0; i < input_elements.length; ++i) {
+            if(!input_elements[i].hasAttribute('data-umlautsConnected')) {
+                addEventListener(input_elements[i]);
+            }
+        }
+    }, 500);
+}
+function typeCharAtCursor(el, newText)
+{
+    var start = el.selectionStart
+    var end = el.selectionEnd
+    var text = el.value
+    var before = text.substring(0, start)
+    var after  = text.substring(end, text.length)
+    el.value = (before + newText + after)
+    el.selectionStart = el.selectionEnd = start + newText.length
+    el.focus()
+}
+
+function addEventListener(element)
+{
+        element.addEventListener("keydown", function (zEvent) {
+            if (zEvent.altKey && zEvent.shiftKey) {
+                switch (zEvent.code) {
+                    case "KeyP":
+                        typeCharAtCursor(this, "Ö");
+                        break;
+
+                    case "KeyQ":
+                        typeCharAtCursor(this, "Ä");
+                        break;
+
+                    case "KeyY":
+                        typeCharAtCursor(this, "Ü");
+                        break;
+                }
+            } else if (zEvent.altKey) {
+                switch (zEvent.code) {
+                    case "KeyP":
+                        typeCharAtCursor(this, "ö");
+                        break;
+
+                    case "KeyQ":
+                        typeCharAtCursor(this, "ä");
+                        break;
+
+                    case "KeyY":
+                        typeCharAtCursor(this, "ü");
+                        break;
+
+                    case "KeyS":
+                        typeCharAtCursor(this, "ß");
+                        break;
+                }
+            }
+        });
+
+        element.setAttribute("data-umlautsConnected", true);
+}
+
+
+(function() {
+    'use strict';
+
+    liveInputWatch();
+})();
---
+++ readme.md
@@ -0,0 +1,52 @@
+# German Umlauts Typer
+
+This script allows you to type German Umlauts easily without using number keys. You can type these umlauts following US international keyboard key's combination. Currently, it only works with memrise.com and is tested on windows only. 
+
+# Other Alternates of Typing Umlauts
+
+  - You can always use Alt + ### keys, more details [here][method1]
+  - You can add [US international keyboard][method2]. 
+
+
+# Why use this script instead?
+
+  - Typing umlauts using Alt+ NUMKeys is pain in the ass. You have to move your typing hand to another world. 
+  - US international keyboard allows to type several other characters with umlauts but it also disturbs existing keys like single and double quotes, carrot character, and some others. 
+  
+
+# What are key combinations for Umlauts
+
+| Key Combination | Output |
+| ------ | ------ |
+| Alt+Q | ä |
+| Alt+Shit+Q | Ä |
+| Alt+Y | ü |
+| Alt+Shift+Y | Ü |
+| Alt+P | ö |
+| Alt+Shit+P | Ö |
+| Alt+S | ß |
+  
+It is recommended to use right alt key and use your right hand thumb to press alt key for good typing practice. May be at some later point, I will tell you why it is better. 
+
+# How to use this script?
+
+  1. Install [TemperMonkey][guide1] Chrome extension. [Watch Video][video1]
+  2. [Click here][guide2] and TemperMonkey will ask you to install this script. Click install button and done!!!
+
+# Enhancements and Suggestions
+Use issues section to provide me your enhancement suggestions and feedback. 
+
+License
+----
+
+MIT
+
+
+**Free Software, Hell Yeah!**
+
+   [method1]: <https://forum.duolingo.com/comment/3635953/An-easy-way-to-type-the-Umlaut-letters>
+   [guide1]: <https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo//Open>
+   [video1]: <https://www.youtube.com/watch?v=cu4XeYtqXbM>
+   [guide2]: <http://daringfireball.net/projects/markdown/>
+   
+
